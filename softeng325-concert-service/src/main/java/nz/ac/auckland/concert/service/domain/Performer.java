a50639f53a015ad652bd109e2f62f96c8fdb1d2b
Nathan Cairns <nathancairns777@gmail.com>
2018-09-03 15:26:44 +1200

    Create performer domain object

--- softeng325-concert-service/src/main/java/nz/ac/auckland/concert/service/domain/Performer.java
+++ softeng325-concert-service/src/main/java/nz/ac/auckland/concert/service/domain/Performer.java
@@ -1,5 +1,7 @@
 package nz.ac.auckland.concert.service.domain;
 
+import nz.ac.auckland.concert.common.types.Genre;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -11,7 +13,35 @@ public class Performer {
     @GeneratedValue
     private long id;
 
+    private Genre genre;
+    private String imageName;
+    private String name;
+
     public long getId() {
         return id;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+
+    public String getImageName() {
+        return imageName;
+    }
+
+    public void setImageName(String imageName) {
+        this.imageName = imageName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
