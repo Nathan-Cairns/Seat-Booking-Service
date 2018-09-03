2db442a7366aaf253fd4ca102b17db6120dbf656
Nathan Cairns <nathancairns777@gmail.com>
2018-09-03 15:30:18 +1200

    Create concert domain obj

--- softeng325-concert-service/src/main/java/nz/ac/auckland/concert/service/domain/Concert.java
+++ softeng325-concert-service/src/main/java/nz/ac/auckland/concert/service/domain/Concert.java
@@ -3,6 +3,8 @@ package nz.ac.auckland.concert.service.domain;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.time.LocalDateTime;
+import java.util.Set;
 
 @Entity
 public class Concert {
@@ -11,7 +13,27 @@ public class Concert {
     @GeneratedValue
     private long id;
 
+    private String title;
+    private Set<LocalDateTime> dates;
+    private Set<Performer> performers;
+
     public long getId() {
         return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Set<LocalDateTime> getDates() {
+        return dates;
+    }
+
+    public Set<Performer> getPerformers() {
+        return performers;
     }
 }
